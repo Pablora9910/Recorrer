2f5d41c28219b25c232c956707ee486449db9248
Pablo <Pablo@DESKTOP-GAEHFSM>
2020-12-10 14:13:17 +0100

    Primer

---
+++ src/es/studium/Recorre/Recorre.java
@@ -0,0 +1,13 @@
+package es.studium.Recorre;
+
+public class Recorre
+{
+	public static void main(String[] args)
+	{
+		String cadena = "ABCDEFGHIJKLMNOPQRSTUVWVYZ";
+		for (int i=0;i<cadena.length();i++) 
+		{
+			System.out.println(cadena.charAt(i));
+		}
+	}
+}
